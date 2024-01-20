890df4b94fc03b73b16b15591f2ea8ab17c89b08
Muhammad Sohaib Ahmed <mmsohaibahmed@gmail.com>
2024-01-20 16:57:14 +0500

    Made basic profile page design

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <main className='w-full flex justify-between bg-white p-4'>
+    <main className='w-full flex justify-between bg-white p-4 border-b-2'>
       <h2 className='text-xl cursor-pointer'>
         Socket Chat
       </h2>
