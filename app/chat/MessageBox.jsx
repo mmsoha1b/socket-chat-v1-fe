d660e609a5e9bfa7abbfe52e9584f5f282e04e81
Muhammad Sohaib Ahmed <mmsohaibahmed@gmail.com>
2024-01-20 12:55:45 +0500

    Made basic chat page design

---
+++ app/chat/MessageBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MessageBox() {
+  return (
+    <div className='flex gap-2'>
+      <div className='w-8 h-8 rounded-full bg-pink-100'></div>
+      <div className='bg-blue-300 min-h-14 p-4 rounded-lg  w-fit max-w-prose'>
+      </div>
+    </div>
+  )
+}
--- app/chat/page.jsx
+++ app/chat/page.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import MessageBox from './MessageBox'
+import { FaPaperPlane } from "react-icons/fa";
 
 export default function Chat() {
   return (
     <div className='flex flex-col relative items-center py-[5dvh] '>
-      <div className='w-[95%]  bg-slate-200 h-[80dvh] overflow-auto shadow-lg rounded-lg'>
+      <div className='w-[95%]  bg-slate-200 h-[80dvh] overflow-auto shadow-lg rounded-lg p-4'>
+        <div className=' mb-16 flex flex-col gap-4'>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        <MessageBox/>
+        </div>
       </div>
-        <div className='w-[95%] absolute bottom-16 min-h-20 bg-white shadow-lg rounded-full'></div>
+       <div className='w-[95%] relative bottom-16 flex min-h-20  bg-white shadow-lg rounded-full'>
+        <div className='w-20 rounded-s-full bg-slate-100'></div>
+        <input className='w-20 px-4 text-xl  flex-grow '></input>
+        <div className='w-20 rounded-e-full bg-slate-100 flex  items-center cursor-pointer'>
+          <FaPaperPlane size={30} className='ml-4'/>
+        </div>
+      </div>
     </div>
   )
 }
