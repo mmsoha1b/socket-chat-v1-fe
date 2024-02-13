01a98e8f7d2547ad7bc3867ac42fcbddd0e5f3df
Muhammad Sohaib Ahmed <mmsohaibahmed@gmail.com>
2024-02-13 14:09:47 +0500

    m

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
     <main className='w-full flex justify-between bg-white p-4 border-b-2'>
-      <h2 className='text-xl cursor-pointer'>
-        Socket Chat
-      </h2>
+      <h2 className='text-xl cursor-pointer'>Socket Chat</h2>
     </main>
-  )
+  );
 }
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,103 +1,126 @@
-"use client"
+"use client";
 
-import { Button, PasswordInput, TextInput } from '@mantine/core'
-import { useForm } from '@mantine/form'
-import Link from 'next/link'
-import React from 'react'
+import { Button, PasswordInput, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import Link from "next/link";
+import React from "react";
 
 export default function Signup() {
   const form = useForm({
-    initialValues:{
-      email:"",
-      password:"",
-      confirmPassword:"",
-      firstName:"",
-      lastName:"",
-      userName:"",
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      firstName: "",
+      lastName: "",
+      userName: "",
     },
     validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-      password:(value)=>value.length>5?null:"Password too short",
-      confirmPassword:(value,values)=>value===values.password?null:"Passwords should match",
-      firstName:(value)=>value.length>0?null:"First name cant be empty",
-      secondName:(value)=>value.length>0?null:"Second name cant be empty"
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) => (value.length > 5 ? null : "Password too short"),
+      confirmPassword: (value, values) =>
+        value === values.password ? null : "Passwords should match",
+      firstName: (value) =>
+        value.length > 0 ? null : "First name cant be empty",
+      secondName: (value) =>
+        value.length > 0 ? null : "Second name cant be empty",
     },
-  })
+  });
   return (
-    <main className='w-1/2 relative top-[50vh]  left-1/4 -translate-y-2/3 '>
+    <main className='w-1/2 relative  left-1/4  mt-auto mb-auto'>
       <div className='bg-white w-full min-h-[50vh] p-8 flex justify-center shadow-xl rounded-xl'>
         <div className='w-full'>
           <form
-          onSubmit={form.onSubmit((values)=>console.log(values))}
-          className='flex w-full flex-col  pt-6 '
+            onSubmit={form.onSubmit((values) => console.log(values))}
+            className='flex w-full flex-col  pt-6 '
           >
             <div className='flex justify-between'>
               <TextInput
-                  withAsterisk
-                  label="First Name"
-                  placeholder='Joe'
-                  variant='filled'
-                  radius="md"
-                  description="Enter first name"
-                  size="md"
-                  className='w-96 mb-8'
-                  {...form.getInputProps("firstName")}
-                  />
-                  <TextInput
-                  withAsterisk
-                  label="Last Name"
-                  placeholder='Doe'
-                  variant='filled'
-                  radius="md"
-                  description="Enter last name"
-                  size="md"
-                  className='w-96 mb-8'
-                  {...form.getInputProps("lastName")}
-                  />
+                withAsterisk
+                label='First Name'
+                placeholder='Joe'
+                variant='filled'
+                radius='md'
+                description='Enter first name'
+                size='md'
+                className='w-96 mb-8'
+                {...form.getInputProps("firstName")}
+              />
+              <TextInput
+                withAsterisk
+                label='Last Name'
+                placeholder='Doe'
+                variant='filled'
+                radius='md'
+                description='Enter last name'
+                size='md'
+                className='w-96 mb-8'
+                {...form.getInputProps("lastName")}
+              />
             </div>
             <TextInput
-                withAsterisk
-                label="Username"
-                placeholder='xXnewcooluserXx'
-                variant='filled'
-                radius="md"
-                description="Enter a username"
-                size="md"
-                className='w-full mb-8'
-                {...form.getInputProps("lastName")}
-                />
-              <PasswordInput
-                withAsterisk
-                label="Password"
-                placeholder='At least 6 chaarcters'
-                variant='filled'
-                radius="md"
-                description="Enter a password"
-                size="md"
-                className='w-full mb-8'
-                {...form.getInputProps("password")}
-                />
-                <PasswordInput
-                withAsterisk
-                label="Retype password"
-                placeholder='At least 6 chaarcters'
-                variant='filled'
-                radius="md"
-                description="Reneter pasword"
-                size="md"
-                className='w-full mb-8'
-                {...form.getInputProps("confirmPassword")}
-                />
+              withAsterisk
+              type='email'
+              label='Email'
+              placeholder='email@domain.com'
+              variant='filled'
+              radius='md'
+              description='Enter your email'
+              size='md'
+              className='w-full mb-8'
+              {...form.getInputProps("email")}
+            />
+            <TextInput
+              withAsterisk
+              label='Username'
+              placeholder='xXnewcooluserXx'
+              variant='filled'
+              radius='md'
+              description='Enter a username'
+              size='md'
+              className='w-full mb-8'
+              {...form.getInputProps("lastName")}
+            />
+            <PasswordInput
+              withAsterisk
+              label='Password'
+              placeholder='At least 6 chaarcters'
+              variant='filled'
+              radius='md'
+              description='Enter a password'
+              size='md'
+              className='w-full mb-8'
+              {...form.getInputProps("password")}
+            />
+            <PasswordInput
+              withAsterisk
+              label='Retype password'
+              placeholder='At least 6 chaarcters'
+              variant='filled'
+              radius='md'
+              description='Reneter pasword'
+              size='md'
+              className='w-full mb-8'
+              {...form.getInputProps("confirmPassword")}
+            />
 
-              <Button type="submit" className='bg-blue-500 w-32 rounded-lg' size='md'>
-                Sign Up
-              </Button>
+            <Button
+              type='submit'
+              className='bg-blue-500 w-32 rounded-lg'
+              size='md'
+            >
+              Sign Up
+            </Button>
           </form>
-        <p className='mt-10  text-sm'>
-          Already have an accout? <Link href="/signin" className='link'> Sign in</Link>
-        </p>
+          <p className='mt-10  text-sm'>
+            Already have an accout?{" "}
+            <Link href='/signin' className='link'>
+              {" "}
+              Sign in
+            </Link>
+          </p>
         </div>
       </div>
     </main>
-  )
+  );
 }
